9823fe56085c72747b44862a85ca48000f6c9bfc
inesantos29 <ines.arts@hotmail.com>
2019-04-19 18:15:09 +0100

    Stateless function component to SearchInput component

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import "./style.css"
 
-class SearchInput extends Component {
-    render() {
-        const { onChange } = this.props;
-
-        return (
-            <div className="search">
-                <input className="search__input" type="text" onChange={(e) => onChange(e.target.value)} placeholder="Search here..."/>
-                <button className="search__btn" type="submit">Search</button>
-            </div>
-        )
-    }
-}
+const SearchInput = ({ onChange }) => (
+    <div className="search">
+        <input className="search__input" type="text" onChange={(e) => onChange(e.target.value)} placeholder="Search here..."/>
+        <button className="search__btn" type="submit">Search</button>
+    </div>
+);
 
 export default SearchInput;
