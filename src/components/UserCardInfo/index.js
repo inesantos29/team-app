500a0fd246a1947af8ab64f63100b476a2b7f8ea
Inês dos Santos <ines.santos@cartrack.com>
2020-11-03 22:15:08 +0000

    Refactored UserCardInfo component

--- src/components/UserCardInfo/index.js
+++ src/components/UserCardInfo/index.js
@@ -1,1 +1,18 @@
-export { default as UserCardInfo } from "./UserCardInfo";
+import React from 'react';
+import styled from 'styled-components'
+
+
+export const UserCardInfo = ({ children, emoji }) => (
+    <Info>
+        <Emoji>{emoji}</Emoji>
+        {children}
+    </Info>
+);
+
+const Info = styled.p`
+    margin: 12px 0 12px; 
+`
+
+const Emoji = styled.span`
+    padding-right: 1rem;    
+`
