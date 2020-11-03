1fc0dbd7d8b3131fecbc878fdcb2ad73586f5815
Inês dos Santos <ines.santos@cartrack.com>
2020-11-03 22:16:01 +0000

    Refactored App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React, { Component } from 'react';
-import './App.css';
-import { UserCardList } from "./components/UserCardList";
+import React from 'react'
+import styled from 'styled-components'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="team-app">
-        <h1>Team App</h1>
-        <UserCardList />
-      </div>
-    );
-  }
+import  GlobalStyle  from './globalStyle'
+import { UserCardList } from './components/UserCardList'
+
+function App() {
+  return (
+    <>
+      <GlobalStyle/>
+      <Container>
+        <Title>Team App</Title>
+        <UserCardList /> 
+      </Container>
+  </>
+  )
 }
 
-export default App;
+export default App
+
+
+const Container = styled.main`
+  max-width: 1366px;
+  margin: 0 auto;
+  padding: 40px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Title = styled.h1`
+ font-size: 30px;
+ margin-bottom: 50px;
+`
+
