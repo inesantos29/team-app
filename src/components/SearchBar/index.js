3136110e4e94f58a3d1ec1835b6c32c028cc1fd7
Inês dos Santos <ines.santos@cartrack.com>
2020-11-03 22:13:07 +0000

    refactored SearchBar component

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+export const SearchBar = ({onChange}) => (
+
+    <SearchForm>
+        <SearchInput 
+            type="text"
+            placeholder="Search by name and email..."
+            onChange={(e) => onChange(e.target.value)}>
+        </SearchInput>
+    </SearchForm>
+)
+
+
+const SearchForm = styled.form`
+    display: flex;
+    width: 100%;
+`
+
+const SearchInput = styled.input`
+    padding: 1em;
+    flex-grow: 1;
+    font-size: inherit;
+    border: none;
+    background-color: #EEE;
+    border-radius: 3px;
+
+
+    &:focus{
+        outline: 0;
+        background: #fff;
+        box-shadow: 0 0 2px rgba(0,0,0,.8);
+    }
+
+    &::-webkit-input-placeholder {
+        color: #999;
+        font-style: italic;
+    }
+
+    &::-moz-placeholder {
+        color: #999;
+        font-style: italic;
+    }
+
+    &::-ms-input-placeholder {
+        color: #999;
+        font-style: italic;
+    }
+
+`
+
+
+
+
+
+
+
+
